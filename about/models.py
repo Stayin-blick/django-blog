cfeab320844a8299dd8f8bc88656c4c98a312cb8
Azeez Bello <azeezbello75@gmail.com>
2024-11-19 06:47:12 +0000

    deployment on heroku

--- about/models.py
+++ about/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
