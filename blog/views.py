e151c82198c2f30487a2b865489ca73b16858074
Azeez Bello <azeezbello75@gmail.com>
2024-11-20 06:52:36 +0000

    comment count and status add and dtl tags

--- blog/views.py
+++ blog/views.py
@@ -23,4 +23,12 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post},)
+    return render(
+        request,
+        "blog/post_detail.html", 
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
+    )
