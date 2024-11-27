4890eec716ded68bbbffd334d6321e66cab1a08b
Azeez Bello <azeezbello75@gmail.com>
2024-11-27 07:18:10 +0000

    add docstrings

--- about/models.py
+++ about/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class About(models.Model):
+    """
+    Stores a single about me text.
+    """
     profile_image = CloudinaryField('image', default='placeholder')
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
@@ -12,6 +15,9 @@ class About(models.Model):
 
 
 class CollaborateRequest(models.Model):
+    """
+    Stores a single collabotatiion request message.
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
--- about/views.py
+++ about/views.py
@@ -4,7 +4,18 @@ from .models import About
 from .forms import CollaborateForm
 
 def about_me(request):
-
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration requests
+    Displays an individual instance of :model:`about.About`.
+    **Conrext**
+    ``about``
+        the most recent instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`.
+    **Template**
+    :template:`about/about.html`
+    """
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
